1f46b958293a7bb6cf850ae318b95308df155265
Carl-W <carl.igelstroem@tiket.com>
2020-05-01 10:34:10 +0800

    devices added.

--- website/src/devices.js
+++ website/src/devices.js
@@ -1,6 +1,36 @@
 export const devices = [
   {
     brand: "Google",
+    model: "Pixel 3A",
+    size: 6.0,
+    screenRatio: "long",
+    xDimension: 1080,
+    yDimension: 2220,
+    githubPath:
+      "https://github.com/ugglr/android-virtual-devices/blob/master/google/pixel3a.xml",
+  },
+  {
+    brand: "Google",
+    model: "Pixel 3A XL",
+    size: 6.0,
+    screenRatio: "long",
+    xDimension: 1080,
+    yDimension: 2160,
+    githubPath:
+      "https://github.com/ugglr/android-virtual-devices/blob/master/google/pixel3aXL.xml",
+  },
+  {
+    brand: "Google",
+    model: "Pixel 3 XL",
+    size: 6.3,
+    screenRatio: "long",
+    xDimension: 1440,
+    yDimension: 2960,
+    githubPath:
+      "https://github.com/ugglr/android-virtual-devices/blob/master/google/pixel3XL.xml",
+  },
+  {
+    brand: "Google",
     model: "Pixel XL",
     size: 5.5,
     screenRatio: "notlong",
